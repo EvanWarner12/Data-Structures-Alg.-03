de49d214532a33e005ee54c7268da654526a9f0f
EvanWarner12 <74017439+EvanWarner12@users.noreply.github.com>
2022-04-19 15:01:50 -0400

    Add files via upload

---
+++ Linked List/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> birds = new LinkedList<String>();
+        birds.add("Cardinal");
+        birds.add("Bald Eagle");
+        birds.add("Blue Jay");
+        birds.add("Wood Pecker");
+        System.out.println(birds);
+    }
+}
